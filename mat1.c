e66f92a5463251ba49722d1a1d88659d681de21d
marcialwushu <marcialwushu@gmail.com>
2016-06-20 16:40:13 -0300

    matriz_2

---
+++ mat1.c
@@ -0,0 +1,31 @@
+#include <stdio.h
+
+int main(){
+
+	int mat[2][2];
+	int i,j,maior;
+
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			printf("Digite valor linha %d e coluna %d da mtatriz: ",i,j);
+			scanf("%d",&mat[i][j]);
+		}
+	}
+
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			if((i && j)==0){
+				maior=mat[i][j];
+			}
+			if(mat[i][j]>maior){
+				maior=mat[i][j];
+			}
+		}
+	}
+
+	printf("%d\n",maior);
+
+	return 0;
+}
+
+
